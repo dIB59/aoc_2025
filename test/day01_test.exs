6229cd72eb2f43a3290aace83f85ef6ae11d6423
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-01 21:45:47 +0100

    update test

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -5,15 +5,6 @@ defmodule Day01Test do
   test "rotate/1 returns the correct remainder" do
     Aoc.Day01.parseData()
 
-    # 100 rem 3 should be 1
-    assert Aoc.Day01.rotate(3) == 1
-
-    # 100 rem 7 should be 2
-    assert Aoc.Day01.rotate(7) == 2
-
-    # 100 rem 100 should be 0
-    assert Aoc.Day01.rotate(100) == 0
-
     Aoc.Day01.execute()
   end
 end
