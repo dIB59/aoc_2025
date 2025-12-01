6cbc5fbef4ceb94bb84b6bc1bdf8a9d6dd4792ee
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-01 17:46:35 +0100

    parse it properly

--- lib/aoc/day01.ex
+++ lib/aoc/day01.ex
@@ -3,24 +3,29 @@ defmodule Aoc.Day01 do
   def parseData() do
     case File.read("lib/aoc/day01data.txt") do
       {:ok, contet} ->
-        contet
-        |> String.split("\n")
-        |> Enum.map(fn line -> String.trim(line) end)
-        |> Enum.reject(fn line -> String.length(line) == 0 end)
-        |> Enum.map(fn line ->
-          {first_char, rest_of_line} = String.split_at(line, 1)
+        parsed_list =
+          contet
+          |> String.split("\n")
+          |> Enum.map(fn line -> String.trim(line) end)
+          |> Enum.reject(fn line -> String.length(line) == 0 end)
+          |> Enum.map(fn line ->
+            {first_char, rest_of_line} = String.split_at(line, 1)
 
-          # 2. Parse the number part
-          parsed_number =
-            rest_of_line
-            |> String.trim()
-            |> Integer.parse()
-            |> elem(0)
+            # 2. Parse the number part
+            parsed_number =
+              rest_of_line
+              |> String.trim()
+              |> Integer.parse()
+              |> elem(0)
 
-          # 3. Return a tuple containing the ORIGINAL line AND the parsed number
-          {first_char, parsed_number}
-        end)
-        |> IO.inspect()
+            # 3. Return a tuple containing the ORIGINAL line AND the parsed number
+            {first_char, parsed_number}
+          end)
+
+        {:ok, parsed_list}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
