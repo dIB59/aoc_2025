0add4e5c8ef48f1cea7a7bab1a9148c5ad0abd3c
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-02 18:07:32 +0100

    add tests

---
+++ test/day02_test.exs
@@ -0,0 +1,26 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  doctest Aoc2025
+  alias Aoc.Day02
+
+  describe "is_repeated/1" do
+    test "returns true for two-digit repeated IDs" do
+      # invalid IDs (repeated halves)
+      assert Day02.is_repeated(11) == true
+      assert Day02.is_repeated(22) == true
+    end
+
+    test "returns true for larger repeated ID" do
+      # invalid ID (repeated halves)
+      assert Day02.is_repeated(1_188_511_885) == true
+    end
+
+    test "returns false for even-length but non-repeated number" do
+      assert Day02.is_repeated(1234) == false
+    end
+
+    test "returns false for odd-length numbers" do
+      assert Day02.is_repeated(123) == false
+    end
+  end
+end
