c6c06e916500758b471363faa404eeb2da35365a
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-02 18:07:29 +0100

    implement repeated func

--- lib/aoc/day02.ex
+++ lib/aoc/day02.ex
@@ -1,4 +1,18 @@
 defmodule Aoc.Day02 do
-  def invalid_id() do
+  @spec is_repeated(integer()) :: boolean()
+  def is_repeated(numbers) do
+    n = Integer.to_string(numbers)
+    len_num = String.length(n)
+
+    case rem(len_num, 2) do
+      1 ->
+        false
+
+      0 ->
+        {first, last} = String.split_at(n, Integer.floor_div(len_num, 2))
+        IO.inspect(first == last)
+
+        first == last
+    end
   end
 end
