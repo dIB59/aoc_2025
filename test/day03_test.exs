e9a1c80156522a0e454635e2265d7069c4f027f3
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-03 13:12:40 +0100

    add test for get biggest

--- test/day03_test.exs
+++ test/day03_test.exs
@@ -6,4 +6,9 @@ defmodule Day03Test do
   test "parse data" do
     Day03.parse_data()
   end
+
+  test "get largest" do
+    Day03.get_biggest([1, 2, 2, 4, 5, 7, 9], 2)
+    |> IO.inspect(charlists: :as_lists)
+  end
 end
