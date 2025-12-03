5087bcb806cf30be7682dea010e860f40a271be8
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-03 13:07:27 +0100

    impl get biggest nums from list

--- lib/aoc/day03.ex
+++ lib/aoc/day03.ex
@@ -9,4 +9,15 @@ defmodule Aoc.Day03 do
         |> IO.inspect()
     end
   end
+
+  @spec get_biggest(list(), integer()) :: list()
+  def get_biggest(list, num) do
+    list
+    |> Enum.with_index()
+    |> Enum.sort_by(fn {num, _i} -> num end)
+    |> Enum.reverse()
+    |> Enum.take(num)
+    |> Enum.sort_by(fn {_num, idx} -> idx end)
+    |> Enum.map(fn {num, _i} -> num end)
+  end
 end
